2c47b1de74d432844229c85b20814ee7b55cb1bd
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-13 15:27:44 -0700

    Fixed 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,15 +1,50 @@
 #include <stdio.h>
 
+/**
+ * main - prints 00-99, but prints only smallest combination of two digits
+ *
+ * Description: Uses multiple loops to puts all numbers, while ensuring
+ * the first number is never larger than the second number
+ * Return: returns 0
+ */
 int main(void)
 {
-    int i, ii, j, jj;
+	int i, ii, j, jj;
 
-    i = ii = '0';
-    j = '0';
-    jj = '1';
-
-    puts(i);
-    puts(ii);
-    puts(j);
-    puts(jj);
+	i = ii = j = '0';
+	jj = '1';
+	while (i <= '9')
+	{
+		while (ii < '9')
+		{
+			while (j <= '9')
+			{
+				while (jj <= '9')
+				{
+					putchar(i);
+					putchar(ii);
+					putchar(' ');
+					putchar(j);
+					putchar(jj);
+					if (i != '9' || j != '9' || ii != '8' || jj != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					jj++;
+				}
+				jj = '0';
+				j++;
+			}
+			ii++;
+			j = i;
+			jj = ii + 1;
+		}
+		i++;
+		ii = '0';
+		jj = '1';
+		j = i;
+	}
+	putchar('\n');
+	return (0);
 }
