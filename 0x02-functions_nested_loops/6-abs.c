ab0c8c0b216e377a78847936c22c0791eed5c596
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-18 13:43:11 -0700

    Fixed 6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * _abs - Returns absolute value of number
+ *
+ * @n: int to get absolute value of
+ * Return: Returns absolute value.
+ */
 int _abs(int n)
 {
 	if (n <= 0)
