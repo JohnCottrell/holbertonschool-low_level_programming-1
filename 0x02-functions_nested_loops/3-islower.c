f8d0eaff11a8a0cee9bd85af8baf0d67f410540a
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-18 13:32:50 -0700

    Fixed formatted for 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/**
+ * _islower - returns 1 if is a lowercase letter
+ *
+ * Return: Returns 1 if a lowercase number, returns 0 if not
+ */
 int _islower(int c)
 {
 	c += '0';
