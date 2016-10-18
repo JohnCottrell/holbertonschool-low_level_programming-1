e42f81b6d92fb4d0ccfdff246dc3ed2064a50cd9
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-18 15:50:10 -0700

    Fixed 11 to account for edgecases

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,10 +27,19 @@ void print_to_98(int n)
  */
 void _putnumber(int n)
 {
+
+	if (n == 98)
+	{
+		_putchar('9');
+		_putchar('8');
+		_putchar(10);
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 	}
+
 	if (n <= 0)
 		n = (n - (n * 2));
 	if (n < 10)
@@ -54,12 +63,6 @@ void _putnumber(int n)
 		_putchar(n % 10 + '0');
 		_putchar(',');
 		_putchar(' ');
-	}
-	if (n == 98)
-	{
-		_putchar('9');
-		_putchar('8');
-		_putchar(10);
 	}
 
 }
