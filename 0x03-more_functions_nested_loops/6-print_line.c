6e2e21638c763487fa85701613a18ac8dd2c5049
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-20 16:05:10 -0700

    Adding print line

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -3,12 +3,16 @@
 /**
  * print_line - draws a straight line in the terminal based on input
  *
- * Description:
- * Return:
+ * @n: length of line
+ * Return: void
  */
-int print_line(void)
+void print_line(int n)
 {
-	
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar(10);
 
-	return (0);
 }
