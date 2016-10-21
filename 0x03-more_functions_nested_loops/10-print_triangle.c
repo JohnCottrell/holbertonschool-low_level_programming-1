5d51d05994680a79fd0a804924328d04cbdc20b0
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-20 18:08:58 -0700

    Fixed 10

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -1,14 +1,36 @@
 #include "holberton.h"
 
 /**
- * main -
+ * print_triangle - prints a triangle on the screen
  *
- * Description:
- * Return:
+ * @size: desired size of triangle
+ * Return: void
  */
-int main (void)
+
+void print_triangle(int size)
 {
+	int whitespace, tri, i;
 
+	i = 1;
+	if (size < 0)
+		_putchar('\n');
+	while (size > 0)
+	{
+		whitespace = size - 1;
+		while (whitespace > 0)
+		{
+			_putchar(' ');
+			--whitespace;
+		}
+		tri = 0;
+		while (tri != i)
+		{
+			_putchar('#');
+			tri++;
+		}
+		_putchar('\n');
+		i++;
+		size--;
+	}
 
-	return (0);
 }
