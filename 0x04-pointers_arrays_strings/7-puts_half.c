d42f78e618525c078d390901cd862e29f9b5dcf8
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-25 11:06:28 -0700

    Fixed 7-puts_half.c

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -31,9 +31,8 @@ void puts_half(char *str)
 
 	i = _strlen(str);
 	if (i % 2 != 0)
-		i = (i - 1) / 2;
-	else
-		i /= 2;
+		i++;
+	i /= 2;
 	while (str[i] != '\0')
 		_putchar(str[i++]);
 	_putchar(10);
