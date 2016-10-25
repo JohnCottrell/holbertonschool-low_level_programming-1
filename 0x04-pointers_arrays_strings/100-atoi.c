7053fd4c4b4c4062470be1163d0b922dee8e4eae
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-25 13:04:51 -0700

    Pushing refactored 100

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -53,38 +53,40 @@ long mypow(long base, long exponent)
  */
 int _atoi(char *s)
 {
-	long i, e, neg, places, temp;
-	int retval;
+	long i, j, neg;
+	long temp[100];
+	int retval, k;
 
-	i = 0;
+	i = j = 0;
+	k = 1;
 	neg = 0;
-	while (s[i] < '0'  || s[i] > '9')
+	retval = 0;
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			neg++;
 		if (s[i] == '+')
 			neg--;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				temp[j++] = s[i++] - '0';
+			}
+			break;
+		}
 		i++;
 	}
-
-	e = i;
-
-	while (s[e] >= '0' && s[e] <= '9' && s[e] != '\0')
-		e++;
-	while (s[e] == '\0' || s[e] == ' ')
-		e--;
-	if (e == 0)
-		return 0;
-	retval = s[e--] - '0';
-	places = 1;
-	while (e >= i)
+	j--;
+	while (j >= 0)
 	{
-		temp = s[e--] - '0';
-		retval += temp * mypow(10, places++);
+		retval = retval + (temp[j--] * k);
+		k = k * 10;
 	}
+
 	if (neg > 0)
-	{
 		retval = retval - (retval * 2);
-	}
+
 	return (retval);
+
 }
