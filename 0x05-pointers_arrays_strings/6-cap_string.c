0c6a45674d12c58644857fa13faaa8d0d515cf07
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-27 15:00:02 -0700

    Altered 6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -28,6 +28,8 @@ char *cap_string(char *s)
 	int i;
 
 	i = 0;
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= 32;
 	while (s[i] != '\0')
 	{
 		if (delim(s[i]) == 1 && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
