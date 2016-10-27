59fc571685f77ec358a15c90f738c8a88272ec30
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-27 12:46:36 -0700

    Fixed 0

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,15 @@
  * @src: source string
  * Return: returns finished string
  */
-
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+	int i, j;
 
-i = j = 0;
-while (dest[i] != '\0')
-	i++;
-while (src[j] != '\0')
-	dest[i++] = src[j++];
-dest[i] = '\0';
-
-return (dest);
+	i = j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest);
 }
