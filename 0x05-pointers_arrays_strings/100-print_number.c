7317ee56d0e7b0c75099106e47b7a850367d17b1
Tim Britton <timothy.britton@holbertonschool.com>
2016-10-28 14:19:37 -0700

    Added 100-print_number

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -1,15 +1,58 @@
 #include "holberton.h"
 
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
+ * powten - quick power of ten implementation
  *
- * Description: Longer description of the function)?
  *
- * Return: Description of the returned value
+ *
+ * @n: exponent
+ * Return: returns ten to exponent power
  */
-
-int main(void)
+int powten(int n)
 {
-	return (0);
+	int retval;
+
+	retval = 10;
+	while (n > 0)
+	{
+		retval *= 10;
+		n--;
+	}
+	return (retval);
+}
+
+/**
+ * print_number - prints integers enters as parameters using putchar
+ *
+ *
+ *
+ * @n: integer to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	int sign, power;
+
+	sign = 1;
+	power = powten(8);
+	if (n > 0)
+	{
+		n = n * -1;
+		sign = sign * -1;
+	}
+	if (n != 0)
+	{
+		while (n / power == 0)
+			power /= 10;
+		if (sign == 1)
+			_putchar('-');
+		while (power >= 1)
+		{
+			_putchar(-(n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
+	}
+	else
+		_putchar('0');
 }
