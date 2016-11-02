d45c2439c6aa44cd0a638cdbff386e757b30b280
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-02 13:55:49 -0700

    Fixed style and documentation on four

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,21 +1,19 @@
 #include "holberton.h"
 
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
+ * _pow_recursion - recursive implementation of pow
  *
- * Description: Longer description of the function)?
- *
- * Return: Description of the returned value
+ * @x: base number
+ * @y: exponent to raise number to
+ * Return: finished
  */
-
 int _pow_recursion(int x, int y)
 {
-        if (y == 0)
-		return 1;
+	if (y == 0)
+		return (1);
 	if (y == 1)
-		return x;
+		return (x);
 	if (y < 0)
-		return -1;
-	return x * _pow_recursion(x, --y);
+		return (-1);
+	return (x * _pow_recursion(x, --y));
 }
