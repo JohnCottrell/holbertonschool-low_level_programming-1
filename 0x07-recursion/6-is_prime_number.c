da07b4b357a03c28c19c01e6c804c0c4ea3f3e61
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-02 14:10:35 -0700

    Pushing updated version of six

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,29 +1,35 @@
 #include "holberton.h"
 
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
+ * prime_helper - helper function for is_prime_number
  *
- * Description: Longer description of the function)?
- *
- * Return: Description of the returned value
+ * @n: number to check for primality
+ * @c: counter for prime check value
+ * Return: returns itself when not finished, when finished returns 1 for prime,
+ * or 0 for not
  */
 int prime_helper(int n, int c)
 {
 	if (n % c == 0)
-		return 0;
+		return (0);
 	if (c > (n / 2))
-		return 1;
-	return prime_helper(n, ++c);
+		return (1);
+	return (prime_helper(n, ++c));
 }
 
+/**
+ * is_prime_number - determines if a number is prime
+ *
+ * @n: number to check
+ * Return: returns 1 if prime, 0 if not
+ */
 int is_prime_number(int n)
 {
 	if (n < 0)
 		n *= -1;
 	if (n % 2 == 0)
-		return 0;
+		return (0);
 	if (n == 1)
-		return 0;
-	return prime_helper(n, 3);
+		return (0);
+	return (prime_helper(n, 3));
 }
