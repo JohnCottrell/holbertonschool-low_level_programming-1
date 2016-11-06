e82f81b1f85644f05ac3114fd04ea3fdb1d16644
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-06 00:13:08 -0700

    Pushing zero

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints name of program to stdout
- * @argv: arguments
- * @argc: number of arguments
- * Return: always zero
+ * main - prints our its name followed by a new line
+ * @argc: arguement count
+ * @argv: strings that come after calling function
+ * Return: none
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	if (argc == 1)
+		printf("%s\n", argv[0]);
+	else
+	{
+		argv[0] = argv[4];
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
