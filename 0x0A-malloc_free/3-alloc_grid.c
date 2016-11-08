d6d40d2094310c682bb04fb98b8456e82c494cb1
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-08 15:04:07 -0700

    Repushing 3 with style fixes

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -14,13 +14,11 @@ int **alloc_grid(int width, int height)
 {
 	int **grid, i, j, k;
 
-	grid = (int**)malloc(height*sizeof(int*));
-
+	grid = (int **) malloc(height * sizeof(int *));
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int*)malloc(width*sizeof(int));
+		grid[i] = (int *)malloc(width * sizeof(int));
 	}
-
 	for (j = 0; j < height; j++)
 	{
 		for (k = 0; k < width; k++)
