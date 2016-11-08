aa369ac056dbc9ebd901a333cc905c6b400fd245
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-08 15:02:23 -0700

    Pushing 4

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "holberton.h"
 
+/**
+ * free_grid - frees a created 2d grid
+ *
+ * @grid: grid to free
+ * @height: height of grid
+ * return: void
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
