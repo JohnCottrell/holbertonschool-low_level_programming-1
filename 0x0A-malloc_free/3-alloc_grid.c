fe900590a663a7dbeee3012b04d82440a52e4e62
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-08 23:46:27 -0700

    Pushing 3, properly freeing space

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -14,14 +14,25 @@ int **alloc_grid(int width, int height)
 {
 	int **grid, i, j, k;
 
+	if (width < 0 || height < 0)
+		return (NULL);
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
+		grid[i] = malloc(width * sizeof(int *));
+		if (grid[i] == NULL);
+		{
+			i--;
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
 			return (NULL);
+		}
 	}
 	for (j = 0; j < height; j++)
 	{
