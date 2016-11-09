2ecadf2f7d97d642ab97e886d26c07acfbadfc14
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-09 11:53:38 -0700

    Fixed 5 possibly

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -60,6 +60,10 @@ char *str_concat(char *s1, char *s2)
 	char *new;
 	int i, j, size;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	size = (_strlen(s1) + _strlen(s2) + 1);
 	new = malloc(size);
 	if (new == NULL)
