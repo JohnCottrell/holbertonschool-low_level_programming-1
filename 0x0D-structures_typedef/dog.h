1da37283267b0e154a87171275367bab6a83568e
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-14 13:44:01 -0800

    Repushing dog.h with proper ifndef

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,17 +1,22 @@
-#ifndef dog
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - all your doggy needs in one structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
-#endif
 
-#ifndef dog_t
 typedef struct dog dog_t;
-#endif
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
