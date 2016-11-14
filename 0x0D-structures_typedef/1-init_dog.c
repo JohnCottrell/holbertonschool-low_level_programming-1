ebc68eba9960de515ca366a03083403cf7d13126
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-14 13:46:01 -0800

    Repushing 1 with proper betty fixes

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,9 +1,18 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * init_dog - initializes a dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (dog != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
