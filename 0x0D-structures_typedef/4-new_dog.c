dd827fb8f67d988f35ce5007f2f60a079841a837
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-14 14:28:51 -0800

    Fixed 4 to reflect requirements

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -3,6 +3,50 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a given string
+ *
+ *
+ * @s: string to check the length of
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copies an entire string to a buffer
+ *
+ *
+ * @src: source string to copy
+ * @dest: buffer to copy string into
+ * Return: returns pointer to copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char *retval;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+
+	retval = dest;
+
+	return (retval);
+}
+
+/**
  * new_dog - creates a new dog structure
  *
  * @name: name of dog
@@ -13,12 +57,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *copyname, *copyowner;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	copyname = malloc(sizeof(name));
+	if (copyname == NULL)
+		return (NULL);
+	_strcpy(copyname, name);
+	copyowner = malloc(sizeof(owner));
+	if (copyowner == NULL)
+		return (NULL);
+	_strcpy(copyowner, owner);
+	new_dog->name = copyname;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = copyowner;
 	return (new_dog);
 }
