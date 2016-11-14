46da495949858382141ce8a796df05739a02553f
Tim Britton <timothy.britton@holbertonschool.com>
2016-11-14 13:56:04 -0800

    Betty fixes on 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,14 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * new_dog - creates a new dog structure
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
+ * Return: returns pointer to newly created dog structure
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
