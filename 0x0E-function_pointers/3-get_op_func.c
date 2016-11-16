bd63261c46209410c3662fcf5c5ead3ef1341ba6
ems541a <ems541a@gmail.com>
2016-11-16 15:15:55 -0800

    Pushing 3 to check through code checker

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "calc.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - determines which operation to use for main
+ *
+ * @s: char to check
+ * Return: returns pointer to appropriate function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+
+	int (*p[5]) (int, int);
+	char operator[] = "+-*/%";
+
+	p[0] = op_add;
+	p[1] = op_sub;
+	p[2] = op_mul;
+	p[3] = op_div;
+	p[4] = op_mod;
+
+	i = 0;
+	while (operator[i] != s[0])
+	{
+		i++;
+		if (i > 4)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+	}
+	return (*p[i]);
+}
