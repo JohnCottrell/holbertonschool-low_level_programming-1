6423254261492d0ae435540a8049adb969690550
ems541a <ems541a@gmail.com>
2016-11-16 13:34:30 -0800

    Betty style/doc fixes on 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * int_index - searches for an integer inside an array
+ *
+ * @array: array to search
+ * @size: size of array
+ * @cmp: comparison function
+ * Return: first location of array that matches
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
@@ -10,7 +18,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	while (i < size)
 	{
 		if ((*cmp)(array[i]))
-			return i;
+			return (i);
 		i++;
 	}
 	return (-1);
