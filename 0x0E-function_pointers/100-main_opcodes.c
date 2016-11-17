e4fc7cf37e652b488b97271ce1d403b976efa371
ems541a <ems541a@gmail.com>
2016-11-16 16:06:54 -0800

    Repush

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 	bytes = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 	pf = (unsigned char *)main;
 	i = 0;
