389d1fa208c11058a602e84ae7eb445ae1095e59
ems541a <ems541a@gmail.com>
2016-11-20 14:54:09 -0800

    Changed 3, over max line count

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -17,40 +17,38 @@ void print_all(const char * const format, ...)
 
 	va_start(arguments, format);
 	i = flag = 0;
-	while (format != NULL)
+	while (format == NULL)
+		return;
+	while (format[i] != '\0')
 	{
-		while (format[i] != '\0')
+		flag = 0;
+		switch (format[i])
 		{
-			flag = 0;
-			switch (format[i])
+		case 'c':
+			printf("%c", (char) va_arg(arguments, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(arguments, int));
+			break;
+		case 'f':
+			printf("%f", (float) va_arg(arguments, double));
+			break;
+		case 's':
+			tempstring = va_arg(arguments, char*);
+			if (tempstring != NULL)
 			{
-			case 'c':
-				printf("%c", (char) va_arg(arguments, int));
+				printf("%s", tempstring);
 				break;
-			case 'i':
-				printf("%d", va_arg(arguments, int));
-				break;
-			case 'f':
-				printf("%f", (float) va_arg(arguments, double));
-				break;
-			case 's':
-				tempstring = va_arg(arguments, char*);
-				if (tempstring != NULL)
-				{
-					printf("%s", tempstring);
-					break;
-				}
-				printf("(nil)");
-				break;
-			default:
-				flag = 1;
 			}
-			if (format[i + 1] != '\0' && flag == 0)
-				printf(", ");
-			i++;
+			printf("(nil)");
+			break;
+		default:
+			flag = 1;
 		}
-		break;
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
+		i++;
 	}
-		printf("\n");
-		va_end(arguments);
+	printf("\n");
+	va_end(arguments);
 }
