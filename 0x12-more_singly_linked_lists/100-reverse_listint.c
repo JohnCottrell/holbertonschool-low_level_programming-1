d86ffbc4834792589341da5c1232f3173d84ccab
ems541a <ems541a@gmail.com>
2016-11-30 19:29:58 -0800

    Betty fix for 100

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - reverses a listint_t struct
+ *
+ * @head: pointer to head of a listint_t
+ * Return: Returns a pointer to the new head pointer
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *list, *temp;
 
 	list = 0;
-	while (*head != NULL) //head = 0
+	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		(*head)->next = list;
