4bbb11542fc921683ceb18c876a2dce6e467c25c
wintermanc3r <ems541a@gmail.com>
2016-12-01 00:49:02 -0800

    Minor changes to 3 to attempt to fix 5?

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,14 +15,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	new->next = NULL;
 	if (*head == NULL)
 	{
-		new->next = *head;
 		*head = new;
 	}
 	else
 	{
-		new->next = NULL;
 		temp = *head;
 		while (temp->next != NULL)
 			temp = temp->next;
