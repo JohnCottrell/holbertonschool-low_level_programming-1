a0354cb26b87aeb92ed6cb9034b44a23a98bdf97
wintermanc3r <ems541a@gmail.com>
2016-12-01 21:14:59 -0800

    Retrying 9, removed outside functions and wrote them into function

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,57 +1,6 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node to the end of a listint_t struct
- *
- * @head: head for listint_t struct
- * @n: value to add to new node
- * Return: returns new node
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *temp, *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-	}
-	return (new);
-}
-
-/**
- * add_nodeint - adds a node to the beginning of a listint_t struct
- *
- * @head: head for listint_t struct
- * @n: value to add to new node
- * Return: returns new node
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
  * insert_nodeint_at_index - inserts a node to a given position in a listint_t
  *
  * @head: head for listint_t struct
@@ -68,35 +17,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		return (NULL);
 	walk = *head;
 	len = 0;
-	while (walk != NULL)
-	{
-		walk = walk->next;
-		len++;
-	}
-	if (index >= len)
-		return (NULL);
-	walk = *head;
-	if (index == 0)
-		return (add_nodeint(head, n));
-	if (index == (len - 1))
-		return (add_nodeint_end(head, n));
-	i = 0;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	if (index == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	walk = *head;
+	i = 0;
 	while (i < index - 1)
 	{
-		if (walk->next == NULL && index > 0)
+		walk = walk->next;
+		if (walk == NULL)
 		{
 			free(new);
 			return (NULL);
 		}
-		walk = walk->next;
 		i++;
 	}
 	new->next = walk->next;
 	walk->next = new;
-
 	return (new);
 }
