ab988844fe2503aef2e00df17b9d29fb00863991
wintermanc3r <ems541a@gmail.com>
2016-12-01 22:53:37 -0800

    Cleanup

--- 0x12-more_singly_linked_lists/102-main.c
+++ 0x12-more_singly_linked_lists/102-main.c
@@ -13,8 +13,11 @@ int main(void)
 	listint_t *head;
 	listint_t *head2;
 	listint_t *node;
+	int n;
 
 	head2 = NULL;
+	n = free_listint_safe(&head2);
+	printf("NULL n = %d\n", n);
 	add_nodeint(&head2, 0);
 	add_nodeint(&head2, 1);
 	add_nodeint(&head2, 2);
@@ -24,30 +27,24 @@ int main(void)
 	add_nodeint(&head2, 402);
 	add_nodeint(&head2, 1024);
 	print_listint_safe(head2);
-	node = find_listint_loop(head2);
-	if (node != NULL)
-	{
-		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-	}
-	free_listint_safe(&head2);
 	head = NULL;
 	node = add_nodeint(&head, 0);
 	add_nodeint(&head, 1);
 	add_nodeint(&head, 2);
 	add_nodeint(&head, 3);
 	add_nodeint(&head, 4);
-	add_nodeint(&head, 5);
-	add_nodeint(&head, 6);
-	node->next = add_nodeint(&head, 7);
-	add_nodeint(&head, 98);
+	node->next = add_nodeint(&head, 98);
 	add_nodeint(&head, 402);
 	add_nodeint(&head, 1024);
 	print_listint_safe(head);
-	node = find_listint_loop(head);
-	if (node != NULL)
-	{
-		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-	}
-	free_listint_safe(&head);
+	n = free_listint_safe(&head2);
+	printf("head2n = %d\n", n);
+	n = free_listint_safe(&head);
+	printf("headn = %d\n", n);
+	printf("%p, %p\n", (void *)head2, (void *)head);
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	n = free_listint_safe(&head2);
+	printf("1 n = %d\n", n);
 	return (0);
 }
--- 0x12-more_singly_linked_lists/9-main.c
+++ 0x12-more_singly_linked_lists/9-main.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 	listint_t *head;
+	void *n;
 
 	head = NULL;
 	add_nodeint_end(&head, 0);
@@ -23,8 +24,9 @@ int main(void)
 	add_nodeint_end(&head, 1024);
 	print_listint(head);
 	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 5, 4096);
-	print_listint(head);
+	n = insert_nodeint_at_index(&head, 0, 4096);
+	printf("%p", n);
+	print_listint_safe(head);
 	free_listint2(&head);
 	return (0);
 }
