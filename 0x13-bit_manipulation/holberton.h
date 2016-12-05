f4ed8a86f0edc855b31556f08755082448ba36be
ems541a <ems541a@gmail.com>
2016-12-05 13:52:28 -0800

    Repushing, trying to fix _putchar use

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,5 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <unistd.h>
+
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 unsigned int power_two(int power);
 void print_binary(unsigned long int n);
@@ -8,4 +11,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+
 #endif
