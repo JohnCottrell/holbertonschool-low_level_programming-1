ec1e52b11a453784592d89e3af23ac1474cb533d
ems541a <ems541a@gmail.com>
2016-12-05 15:04:08 -0800

    Betty fixes, etc

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-int main()
+/**
+ * get_endianness - checks the endianness of the processor
+ *
+ * Return: returns 1 for little endian, 0 for big
+ */
+int get_endianness(void)
 {
 	unsigned int i = 1;
-	char *c = (char*)&i;
+	char *c = (char *)&i;
+
 	if (*c)
-		printf("Little endian");
-	else
-		printf("Big endian");
-	getchar();
-	return 0;
-} 
+		return (1);
+	return (0);
+}
