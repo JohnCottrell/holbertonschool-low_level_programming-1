004abec9b13c7917622b75394d08a588c2bc1a0b
ems541a <ems541a@gmail.com>
2016-12-07 22:43:26 -0800

    Typo city

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int file, retval, readval;
 	char *buff;
 
-	file = retval = readval;
+	file = retval = readval = 0;
 	if (filename == NULL)
 		return (0);
 	if (letters == 0)
