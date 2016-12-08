5f89939279234b02fb19dac77ae17cfe03c380b2
wintermanc3r <timothy.britton@holbertonschool.com>
2016-12-08 17:31:41 +0000

    error checking changes

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -11,23 +11,27 @@
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, readval, retval;
-	char *buf;
+	char buf[BUFSIZE];
 
-	buf = malloc(sizeof(char) * BUFSIZE);
 	retval = 1;
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		free(buf);
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDWR);
-	file_to = creat(argv[2], 0664);
+	if (argv[0] == NULL)
+	  {
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+	  }
+	if (argv[1] == NULL)
+	  {                                  dprintf(STDERR_FILENO, "Erro\r: Can't write to file %s\n", argv[2]);                                 exit(99);                                           }      	
+file_from = open(argv[1], O_RDONLY);
+ file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	readval = read(file_from, buf, BUFSIZE);
 	if (readval == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-				free(buf);
 		exit(98);
 	}
 	while (readval != 0)
@@ -36,14 +40,12 @@ int main(int argc, char *argv[])
 		if (retval == -1 || retval != readval)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-					free(buf);
 			exit(99);
 		}
 		readval = read(file_from, buf, BUFSIZE);
 		if (readval == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-					free(buf);
 			exit(98);
 		}
 
@@ -52,14 +54,12 @@ int main(int argc, char *argv[])
 	if (retval == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-				free(buf);
 		exit(100);
 	}
 	retval = close(file_to);
 	if (retval == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-				free(buf);
 		exit(100);
 	}
 	return (1);
