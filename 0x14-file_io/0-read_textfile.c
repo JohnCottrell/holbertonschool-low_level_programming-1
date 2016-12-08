ed4be03759853c6a5c5782de3182d125c8216339
ems541a <ems541a@gmail.com>
2016-12-07 22:24:16 -0800

    Pushing with better error checking

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -14,17 +14,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, retval;
+	int file, retval, readval;
 	char *buff;
 
+	if (filename == NULL)
+		return (-1);
 	buff = malloc(letters * sizeof(char));
+	if (buff = NULL)
+		return (-1);
 	file = open(filename, O_RDWR);
 	if (filename == NULL || file == -1)
 		return (0);
-	retval = read(file, buff, letters);
+	readval = read(file, buff, letters);
 	if (retval == -1)
 		return (0);
 	retval = write(1, buff, retval);
+	if (writeval != readval || retval == -1)
+		return -1;
 	free(buff);
 	return (retval);
 }
