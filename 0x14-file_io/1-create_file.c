ad6d85f83710da3770f09cc638912ff0e1cf204a
ems541a <ems541a@gmail.com>
2016-12-07 21:18:55 -0800

    Error for filename before file opening attempt

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,9 +11,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, i, retval;
-
+	if (filename == NULL)
+		return (-1);
 	file = open(filename, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-	if (filename == NULL || file == -1)
+	if (file == -1)
 		return (-1);
 	i = 0;
 	if (text_content != NULL)
