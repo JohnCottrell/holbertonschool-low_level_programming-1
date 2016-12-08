6b1b2dc96e3afd5b6c8fbeb96881e9e4b74dde29
ems541a <ems541a@gmail.com>
2016-12-07 21:16:34 -0800

    1 properly creates empty files

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,22 +14,17 @@ int create_file(const char *filename, char *text_content)
 
 	file = open(filename, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
 	if (filename == NULL || file == -1)
-	{
 		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		return (1);
-	}
 	i = 0;
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		i++;
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
 	}
 	retval = write(file, text_content, i);
 	if (retval == -1)
-	{
 		return (-1);
-	}
 	return (1);
 }
