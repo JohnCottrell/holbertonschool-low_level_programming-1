3c7d0f4e0aee4797c59b89603418d0dad443adb1
ems541a <ems541a@gmail.com>
2016-12-07 23:08:43 -0800

    Changed STDOUT_FILENO back to 2

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	retval = 1;
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDWR);
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	retval = read(file_from, buf, 1024);
 	if (retval == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	while (retval != 0)
@@ -32,13 +32,13 @@ int main(int argc, char *argv[])
 		retval = write(file_to, buf, retval);
 		if (retval == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 		retval = read(file_from, buf, retval);
 		if (retval == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 
@@ -46,13 +46,13 @@ int main(int argc, char *argv[])
 	retval = close(file_from);
 	if (retval == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", retval);
+		dprintf(2, "Error: Can't close fd %d\n", retval);
 		exit(100);
 	}
 	retval = close(file_to);
 	if (retval == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", retval);
+		dprintf(2, "Error: Can't close fd %d\n", retval);
 		exit(100);
 	}
 	return (1);
