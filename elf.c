9019018fee6a5f7b7bd427a14d667bf6712aadd4
wintermanc3r <ems541a@gmail.com>
2016-12-08 15:40:38 -0800

    Commit for safety

--- elf.c
+++ elf.c
@@ -2,38 +2,51 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 //http://www.jinmei.org/download/unwindtest.c
 //https://github.com/BR903/ELFkickers/blob/master/elftoc/readelf.c
 //https://onyx.boisestate.edu/repos/amit/cs453/lab/process-management/display-elf-headers.c
 
+void printClass(void *ehdr);
+void _perror(char *errmsg, int exitcode);
+int _strlen(char *s);
 void printMagic(void *ehdr);
+void printData(void *header);
+void printVersion(void *header);
+void printOS(void *header);
+void printABIVersion(void *header);
+void printType(void *header);
 int main (int argc, char *argv[])
 {
   int file;
   file = open(argv[1], O_RDONLY);
   if (file == -1)
-    {
-    printf("Error opening file.");
-    perror("");
-    }
+	  _perror("Cannot open file.\n", 98);
   Elf64_Ehdr header;
   read(file, &header, sizeof(Elf64_Ehdr));
-//  if (header.e_ident[0] != 127 || header.e_ident[1] != 'E' ||
-  //     header.e_ident[2] != 'L' || header.e_ident[2] != 'F')
-  // {
-//	  exit(98); /* Not an ELF File! */
-//  }
+  if (header.e_ident[0] != ELFMAG0 || header.e_ident[1] != ELFMAG1 ||
+       header.e_ident[2] != ELFMAG2 || header.e_ident[3] != ELFMAG3)
+   {
+	   _perror("Error: Not an ELF file - it has the wrong magic bytes at the start\n", 98);
+  }
   printf("ELF Header:\n");
   printMagic(&header);
+  printClass(&header);
+  printData(&header);
+  printVersion(&header);
+  printOS(&header);
+  printABIVersion(&header);
+  printType(&header);
 }
 
 void printMagic(void *header)
 {
 	Elf64_Ehdr *ehdr = header;
-	printf("Magic:   ");
+	printf("  Magic:   ");
 	printf("%02x ", ehdr->e_ident[0]);
 	printf("%02x ", ehdr->e_ident[1]);
 	printf("%02x ", ehdr->e_ident[2]);
+	printf("%02x ", ehdr->e_ident[3]);
 	printf("%02x ", ehdr->e_ident[4]);
 	printf("%02x ", ehdr->e_ident[5]);
 	printf("%02x ", ehdr->e_ident[6]);
@@ -45,39 +58,186 @@ void printMagic(void *header)
 	printf("%02x ", ehdr->e_ident[12]);
 	printf("%02x ", ehdr->e_ident[13]);
 	printf("%02x ", ehdr->e_ident[14]);
-	printf("%02x ", ehdr->e_ident[15]);
+	printf("%02x\n", ehdr->e_ident[15]);
 }
 
-void printClass(void *ehdr)
+void printClass(void *header)
 {
-
+	Elf64_Ehdr *ehdr = header;
+	printf("  Class:");
+	printf("                             ");
+	switch (ehdr->e_ident[EI_CLASS])
+	{
+	case ELFCLASS32:
+		printf("ELF32");
+		break;
+	case ELFCLASS64:
+		printf("ELF64");
+		break;
+        case ELFCLASSNONE:
+		printf("Invalid class");
+		break;
+	}
+	printf("\n");
 }
 
-void printData(void *ehdr)
+void printData(void *header)
 {
-
+	Elf64_Ehdr *ehdr = header;
+	printf("  Data:");
+	printf("                              ");
+	switch (ehdr->e_ident[EI_DATA])
+	{
+	case ELFDATANONE:
+		printf("Invalid ELF data");
+		break;
+	case ELFDATA2LSB:
+		printf("2's complement, little endian");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big endian");
+		break;
+	}
+	printf("\n");
 }
-void printVersion(void *ehdr)
+void printVersion(void *header)
 {
-
+	Elf64_Ehdr *ehdr = header;
+	printf("  Version:");
+	printf("                           ");
+	switch (ehdr->e_ident[EI_VERSION])
+	{
+	case EV_NONE:
+		printf("Invalid version");
+		break;
+	case EV_CURRENT:
+		printf("1 (current)");
+		break;
+	}
+	printf("\n");
 }
 
-void printOS(void *ehdr)
+void printOS(void *header)
 {
-
+	Elf64_Ehdr *ehdr = header;
+	printf("  OS/ABI:");
+	printf("                            ");
+	switch (ehdr->e_ident[EI_OSABI])
+	{
+	case ELFOSABI_SYSV:       //UNIX System V ABI.
+		printf("UNIX - System V");
+		break;
+	case ELFOSABI_HPUX:       //HP-UX ABI.
+		printf("UNIX - HP-UX");
+		break;
+	case ELFOSABI_NETBSD:     //NetBSD ABI.
+		printf("UNIX - NetBSD");
+		break;
+	case ELFOSABI_LINUX:      //Linux ABI.
+		break;
+	case ELFOSABI_SOLARIS:    //Solaris ABI.
+		printf("UNIX - Solaris");
+		break;
+	case ELFOSABI_AIX:
+		printf("UNIX - AIX");
+		break;
+	case ELFOSABI_IRIX:       //IRIX ABI.
+		printf("UNIX - IRIX");
+		break;
+	case ELFOSABI_FREEBSD:    //FreeBSD ABI.
+		printf("UNIX - FreeBSD");
+		break;
+	case ELFOSABI_TRU64:      //TRU64 UNIX ABI.
+		printf("UNIX - TRU64");
+		break;
+	case ELFOSABI_MODESTO:
+		printf("Novell - Modesto");
+		break;
+	case ELFOSABI_OPENBSD:
+		printf("UNIX - OpenBSD");
+		break;
+	case ELFOSABI_OPENVMS:
+		printf("VMS - OpenVMS");
+	case ELFOSABI_NSK:
+		printf("HP - Non-Stop Kernel");
+	case ELFOSABI_AROS:
+		printf("AROS");
+	case ELFOSABI_STANDALONE:
+		printf("Standalone App");
+	case ELFOSABI_ARM:
+		printf("ARM");
+	case ELFOSABI_FENIXOS:
+		printf("FenixOS");
+	default:
+		printf("<unknown: %d>", ehdr->e_ident[EI_OSABI]);
+	}
+	printf("\n");
 }
 
-void printABI(void *ehdr)
+void printABIVersion(void *header)
 {
-
+	Elf64_Ehdr *ehdr = header;
+	printf("  ABI Version:");
+	printf("                       ");
+	if (ehdr->e_ident[EI_ABIVERSION] == 0)
+		printf("0");
+	else
+		printf("1");
+	printf("\n");
 }
 
-void printType(void *ehdr)
+void printType(void *header)
 {
-
+	Elf64_Ehdr *ehdr = header;
+	printf("  Type:");
+	printf("                              ");
+	switch (ehdr->e_type)
+	{
+	case ET_NONE:
+		printf("NONE (None)");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)");
+		break;
+	case ET_CORE:
+		printf("CORE (Core file)");
+		break;
+	default:
+		printf("<unknown>: %d", ehdr->e_type);
+	}
+	printf("\n");
 }
 
-void printEntry(void *ehdr)
+void printEntry(void *header)
 {
+	Elf64_Ehdr *ehdr = header;
+}
 
+void _perror(char *errmsg, int exitcode)
+{
+	write(STDERR_FILENO, errmsg, _strlen(errmsg));
+	exit(exitcode);
+}
+
+/**
+ * _strlen - returns the length of a given string
+ *
+ *
+ * @s: string to check the length of
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
