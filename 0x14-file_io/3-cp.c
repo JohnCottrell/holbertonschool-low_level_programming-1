3d280f353d16c0484a4bcf659249dc961905fe85
ems541a <ems541a@gmail.com>
2016-12-07 23:07:37 -0800

    Pushing 3...

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
 #include "holberton.h"
-#define BUF_SIZE 1024
+#include <stdio.h>
 
 /**
  * main - copies a file
@@ -16,21 +11,20 @@
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, retval;
-	char buf[BUF_SIZE];
+	char buf[1024];
 
 	retval = 1;
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDWR);
-	file_to = open(argv[2], O_RDWR | O_CREAT | O_EXCL,
-		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	retval = read(file_from, buf, BUF_SIZE);
+	file_to = open(argv[2], O_RDWR | O_CREAT | O_EXCL, 0674);
+	retval = read(file_from, buf, 1024);
 	if (retval == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	while (retval != 0)
@@ -38,16 +32,28 @@ int main(int argc, char *argv[])
 		retval = write(file_to, buf, retval);
 		if (retval == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 		retval = read(file_from, buf, retval);
 		if (retval == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 
+	}
+	retval = close(file_from);
+	if (retval == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", retval);
+		exit(100);
+	}
+	retval = close(file_to);
+	if (retval == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", retval);
+		exit(100);
 	}
 	return (1);
 }
