d6417426d9a19518ab231bce507a7dabad3ac93c
ems541a <ems541a@gmail.com>
2016-12-07 21:25:42 -0800

    Refixed 1. Whoops

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -12,20 +12,20 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, i, retval;
 
+	retval = file = i = 0;
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
-	i = 0;
 	if (text_content != NULL)
 	{
 		while (text_content[i] != '\0')
 		{
 			i++;
 		}
+		retval = write(file, text_content, i);
 	}
-	retval = write(file, text_content, i + 1);
 	if (retval == -1)
 		return (-1);
 	return (1);
