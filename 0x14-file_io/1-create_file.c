66956132211baa5e477b2b5b77f83045efd09741
ems541a <ems541a@gmail.com>
2016-12-07 21:28:27 -0800

    Changed 1 to work even if unable to create new file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	retval = file = i = 0;
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	file = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
