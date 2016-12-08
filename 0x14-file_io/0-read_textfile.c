b3cbcffb5db85a6ea6327415d507b2e8a50ab7b9
ems541a <ems541a@gmail.com>
2016-12-07 22:39:48 -0800

    Freeing buff on failure

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -21,19 +21,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buff = malloc(letters * sizeof(char));
 	if (buff == NULL)
+	{
+		free(buff);
 		return (0);
+	}
 	file = open(filename, O_RDWR);
 	if (filename == NULL || file == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	readval = read(file, buff, letters);
 	if (readval == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	retval = write(1, buff, readval);
-	if (retval == -1)
+	if (retval == -1 || readval != retval)
+	{
+		free(buff);
 		return (0);
+	}
 	free(buff);
 	readval = close(file);
 	if (readval == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	return (retval);
 }
