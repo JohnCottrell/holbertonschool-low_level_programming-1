87557900346e0d7c6ae60d86f69b80d3e4eca691
wintermanc3r <timothy.britton@holbertonschool>
2016-12-08 06:19:23 +0000

    Numerous fixes on 0, changed the way the buffer works, removed variable size buffer

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -15,15 +15,16 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file, retval;
-	char buff[letters];
+	char *buff;
 
+	buff = malloc(letters * sizeof(char));
 	file = open(filename, O_RDWR);
 	if (filename == NULL || file == -1)
 		return (0);
 	retval = read(file, buff, letters);
 	if (retval == -1)
 		return (0);
-	write(1, &buff, retval);
-
+	retval = write(1, buff, retval);
+	free(buff);
 	return (retval);
 }
