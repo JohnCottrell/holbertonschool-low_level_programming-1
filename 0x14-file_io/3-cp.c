4bcff232487d37e3337af4c2271630d42ce757fe
ems541a <ems541a@gmail.com>
2016-12-08 10:29:31 -0800

    Repushing, fixed NULL error messages

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -17,14 +17,15 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
-	if (argv[0] == NULL)
+	if (argv[1] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
-	if (argv[1] == NULL)
+	if (argv[2] == NULL)
 		dprintf(STDERR_FILENO, "Erro\r: Can't write to file %s\n", argv[2]),
 			exit(99);
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	readval = read(file_from, buf, BUFSIZE);
 	if (readval == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
@@ -45,5 +46,5 @@ int main(int argc, char *argv[])
 	retval = close(file_to);
 	if (retval == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-	return (1);
+	return (0);
 }
