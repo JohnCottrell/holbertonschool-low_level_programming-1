4a3378fc427f334cd34c20cf45506422c6b67f65
wintermanc3r <ems541a@gmail.com>
2016-12-08 18:26:23 -0800

    Cleanup

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -6,4 +6,18 @@
 int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int printClass(void *ehdr);
+void error(char *errmsg, int exitcode);
+int _strlen(char *s);
+void printMagic(void *ehdr);
+int printData(void *header);
+void printVersion(void *header);
+void printOS(void *header);
+void printABIVersion(void *header);
+void printType(void *header);
+void printEntry(void *header);
+void *getHeader(char *argv[]);
+void checkHeader(void *header);
+void bigType(void *header);
+void bigEntry(void *header, int arch);
 #endif
