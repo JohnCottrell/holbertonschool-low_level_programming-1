96f6c899669336c16fabc1f4e190dd4daf5db9d1
wintermanc3r <ems541a@gmail.com>
2017-01-08 12:16:40 -0800

    Documentation on 1

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - returns the length of a dlistint list
+ *
+ * @h: head of dlistint
+ * Return: Returns number of elements in doubly linked list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int i;
