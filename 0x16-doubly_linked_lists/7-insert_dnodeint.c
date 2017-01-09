08fd741b0ac894fcdcd7644aade5070f3dfde105
wintermanc3r <ems541a@gmail.com>
2017-01-08 18:24:02 -0800

    Typo!

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *walk, *newnode, *temp;
 	unsigned int i;
 
-	if (h == NULL && n != 0)
+	if (h == NULL && idx != 0)
 		return (NULL);
 	if (h != NULL)
 		walk = *h;
