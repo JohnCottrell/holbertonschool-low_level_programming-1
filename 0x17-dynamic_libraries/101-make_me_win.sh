86ecf7c561714419d6f065f592c3d4f96e68c400
wintermanc3r <ems541a@gmail.com>
2017-01-10 19:09:44 -0800

    Files for 0x07!

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+wget https://github.com/wintermanc3r/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/wrapper.o -O /tmp/wrapper.o
+export LD_PRELOAD=/tmp/wrapper.o
