c7f292aafeca99f609b8c58bb963dfa5a412b8e9
wintermanc3r <ems541a@gmail.com>
2017-01-12 18:11:30 -0800

    Still not passing...

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- wget https://github.com/wintermanc3r/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/wrapper.so -O /tmp/wrapper.so
+ wget -q -O /tmp/wrapper.so https://github.com/wintermanc3r/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/wrapper.so
  export LD_PRELOAD=/tmp/wrapper.so
