88226a37d7685e81ba125057fda480b148b785ba
wintermanc3r <ems541a@gmail.com>
2017-01-12 17:27:14 -0800

    Changed wrapper name to reflect proper filetype

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/wintermanc3r/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/wrapper.o -O /tmp/wrapper.o
-export LD_PRELOAD=/tmp/wrapper.o
+wget -q https://github.com/wintermanc3r/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/wrapper.so -O /tmp/wrapper.so
+export LD_PRELOAD=/tmp/wrapper.so
