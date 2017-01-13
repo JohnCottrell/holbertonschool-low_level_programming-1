5b1bbfe10e88e4291e53d4275ffb18021613b867
wintermanc3r <ems541a@gmail.com>
2017-01-12 18:06:06 -0800

    Push

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q https://github.com/wintermanc3r/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/wrapper.so -O /tmp/wrapper.so
-export LD_PRELOAD=/tmp/wrapper.so
+ wget https://github.com/wintermanc3r/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/wrapper.so -O /tmp/wrapper.so
+ export LD_PRELOAD=/tmp/wrapper.so
