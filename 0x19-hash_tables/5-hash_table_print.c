2a4cdca7bfcc686c99d5ae33c6b2a4057dd647d3
wintermanc3r <ems541a@gmail.com>
2017-02-04 13:26:44 -0800

    Documentation fixes for 5

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - prints a hash_table_t out
+ *
+ * @ht: hash_table_t to print
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
@@ -8,12 +13,13 @@ void hash_table_print(const hash_table_t *ht)
 	flag = 0;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
-		if (ht->array[i] != NULL)
+		while (ht->array[i] != NULL)
 		{
 			if (flag == 1)
 				printf(", ");
 			printf("'%s': '%s'", (ht->array[i])->key, (ht->array[i])->value);
 			flag = 1;
+			ht->array[i] = ((ht->array[i])->next);
 		}
 	printf("}");
 }
