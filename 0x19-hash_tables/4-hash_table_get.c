c2710b3fe8b5ef8dd3bdf2c1398709cd244cbaf2
wintermanc3r <ems541a@gmail.com>
2017-02-04 12:31:02 -0800

    Pushing fixed 4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,12 +1,19 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - returns a value associated with a key in a ht
+ *
+ * @ht: hash_table_t we're working with
+ * @key: key to search for
+ * Return: Returns pointer to value, or NULL on fail
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int hsh;
 
-	hsh = key_index(key, ht->size);
+	hsh = key_index((unsigned char *) key, ht->size);
 	if (ht->array[hsh])
 		return ((ht->array[hsh])->value);
-	return NULL;
+	return (NULL);
 
 }
