423d3ab380c2997c21760e48135de6eac314c747
ems541a <ems541a@gmail.com>
2017-03-25 23:54:15 -0700

    Null checks for 2 as well.

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -12,7 +12,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+		return (NULL);
 	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 	if (parent->right == NULL)
 		parent->right = new;
 	else
